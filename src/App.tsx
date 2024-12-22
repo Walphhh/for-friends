b3602795c3a11f5e4f5a71a98c6577e766404e01
Walphhh <93171385+Walphhh@users.noreply.github.com>
2024-12-22 23:15:07 +1100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ChristmasTree from "./components/ChristmasTree";
+
+const App = () => {
+  return (
+    <div>
+      <ChristmasTree />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ChristmasTree.tsx
@@ -0,0 +1,11 @@
+import GIF from "../assets/ChristmasTree.gif";
+
+const ChristmasTree = () => {
+  return (
+    <div>
+      <img src={GIF} alt="Christmas Tree GIF" />
+    </div>
+  );
+};
+
+export default ChristmasTree;
